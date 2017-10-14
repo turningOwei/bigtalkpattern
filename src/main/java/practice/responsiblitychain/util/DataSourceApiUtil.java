26a99bcd8445490ec192f462c73fd53a90e8715c
002465 <weipeng@xinhehui.com>
2017-10-14 16:28:00 +0800

    是否头一个节点,是否末尾节点

--- src/main/java/practice/responsiblitychain/util/DataSourceApiUtil.java
+++ src/main/java/practice/responsiblitychain/util/DataSourceApiUtil.java
@@ -7,9 +7,11 @@ import practice.responsiblitychain.response.DataSourceResponse;
  * Created by 002465 on 2017/10/13.
  */
 public abstract class DataSourceApiUtil <A extends DataSourceRequest,B extends DataSourceResponse>{
-    private boolean isFinalNode;
+    private boolean isStartNode;
     /**责任链传递值*/
     protected DataSourceApiUtil successor;
+
+    private boolean isTailNode;
     public abstract B getData(A request);
 
     public DataSourceApiUtil getSuccessor() {
@@ -20,11 +22,19 @@ public abstract class DataSourceApiUtil <A extends DataSourceRequest,B extends D
         this.successor = successor;
     }
 
-    public boolean isFinalNode() {
-        return isFinalNode;
+    public boolean isStartNode() {
+        return isStartNode;
     }
 
-    public void setFinalNode(boolean finalNode) {
-        isFinalNode = finalNode;
+    public void setStartNode(boolean startNode) {
+        isStartNode = startNode;
+    }
+
+    public boolean isTailNode() {
+        return isTailNode;
+    }
+
+    public void setTailNode(boolean tailNode) {
+        isTailNode = tailNode;
     }
 }
